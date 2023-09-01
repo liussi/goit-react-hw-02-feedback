5adf55fcdbc945d106dfb6bb479939c7f27db0a4
liussi <125599487+liussi@users.noreply.github.com>
2023-09-02 00:03:19 +0300

    fix

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -4,7 +4,7 @@ import { List } from "./Statistics.styled";
 const Statistics = ({good, neutral, bad, total, positivePercentage}) => (
 <>
             <List>Good: { good}</List>
-            <List>Neatral: { neutral}</List>
+            <List>Neutral: { neutral}</List>
             <List>Bad: {bad }</List>
             <List>Total: {total }</List>
             <List>Positive feedback : { Math.floor(positivePercentage)}%</List>
