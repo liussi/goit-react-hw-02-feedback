ebc2286e4d77ac226fa99c749f52e5511ede6be0
liussi <125599487+liussi@users.noreply.github.com>
2023-09-05 10:12:16 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from "prop-types";
 import Statistics from './Statistics/Statistics'
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Section from './Section/Section';
